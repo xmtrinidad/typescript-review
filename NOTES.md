7da344e1c658cd42c3aeea76d8d088747274b1f4
Xavier <xmtrinidad@gmail.com>
2021-11-20 09:18:06 -0700

    went through section 1

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# Notes from the Udemy TS tutorial
+
+* TS is a JavaScript superset
+  * Adds new features and advantages to JavaScript
+  * Browsers can't excute JavaScript, needs a compiler
+  * The compiler doesn't add what doesn't exist in JavaScript, but it compiles it in a way that's cleaner and easier to use
+
+
+
---
+++ Section-1-Getting-Started/first-project/js-only.js
@@ -0,0 +1,13 @@
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1");
+// const input2 = document.getElementById("num2");
+
+
+// // This is an error prone example of adding two numbers in vanilla javascript because these numbers could both be strings and give undesired results
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function() {
+//   console.log(add(input1.value, input2.value));
+// });
