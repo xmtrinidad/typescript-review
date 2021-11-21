0c6b25ebd8b500ed6e35059fed71e48fa4ef1ab8
Xavier <xmtrinidad@gmail.com>
2021-11-21 07:57:27 -0700

    objects example

---
+++ Section-2/NOTES.md
@@ -0,0 +1,7 @@
+# TypeScript Basics & Basic Types
+
+## Core Types
+
+* number - same as js
+* string - same as js
+* boolean - same as js
--- course_project/app.ts
+++ course_project/app.ts
@@ -1,1 +1,19 @@
-console.log('test thing!!!');
+// Looks like an object but this is an "object type"
+const person = {
+  name: 'max',
+  age: 30
+};
+
+// This is the actual type of object
+// const person1: object = {
+//   name: 'person',
+//   age: '30'
+// }
+
+const person1: {name: string; age: number;} = {
+  name: 'person',
+  age: 30
+}
+
+// nickname doesn't exist on the person object type
+// console.log(person.nickname)
