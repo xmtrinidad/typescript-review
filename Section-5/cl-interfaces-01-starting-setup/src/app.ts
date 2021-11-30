e6162a37e40b50194f785bc6bed4f6f46029896f
Xavier <xmtrinidad@gmail.com>
2021-11-29 20:31:22 -0700

    started classes

--- Section-5/cl-interfaces-01-starting-setup/src/app.ts
+++ Section-5/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,16 +1,68 @@
 class Department {
-  name: string;
+  protected employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  // Short hand initialization
+  constructor(private readonly id: string, public name: string) {
+    
   }
 
   // Extra type safety to ensure the instance of this class is being used
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+  
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-accounting.describe();
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push('Person');
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new Department('d1', 'Accounting');
+// const it = new ITDepartment('d1', ['Xavier']);
+
+// it.addEmployee('Bruh');
+// it.addEmployee('Thing');
+
+// This won't work because employees is private
+// accounting.employees[2] = 'Anna';
+
+// it.describe();
+// it.printEmployeeInformation();
+
+// console.log(it);
+
+const accounting = new AccountingDepartment('d2', [])
+
+accounting.addReport('Something went wrong...');
