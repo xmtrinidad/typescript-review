05d74c03226d690ba05b6c18799dacc51e683b92
Xavier <xmtrinidad@gmail.com>
2021-12-05 21:19:31 -0700

    added generic class example

--- Section-7/src/app.ts
+++ Section-7/src/app.ts
@@ -9,3 +9,33 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then(data => {
   data.split(' ');
 });
+
+// Generic Class
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorge = new DataStorage<string>();
+textStorge.addItem('Thing');
+textStorge.addItem('test');
+textStorge.removeItem('Thing');
+console.log(textStorge.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(1);
+console.log(numberStorage.getItems());
+
